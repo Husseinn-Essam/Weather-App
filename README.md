5d8da11f430d997a0382822ea47f5b86f6b2f62a
Hussein <112550601+Husseinn-Essam@users.noreply.github.com>
2023-06-10 13:30:03 +0300

    background image feature + readme

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# Weather-App
-Weather App  
+# Weather App
+
+This is a weather app that displays real-time weather information for a given location. The app features a weather card that dynamically updates with the current weather conditions, including temperature, humidity, wind speed, date, and time.
+
+## Features
+
+- Displays the current weather conditions for a specific location.
+- Changes the background image based on the weather condition.
+- Shows the temperature in Celsius.
+- Includes additional information such as humidity and wind speed.
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+- WeatherAPI
+
+## Usage
+
+1. Enter the name of a city in the search bar.
+2. Click the "Search" button.
+3. The weather card will update with the current weather conditions for the specified city.
+4. The background image will change based on the weather condition.
+5. The card will display the temperature in Celsius, humidity, wind speed, date, and time.
+
--- src/index.js
+++ src/index.js
@@ -1,43 +1,55 @@
 import './styles/main.scss';
 //import "@babel/polyfill";
 const searchBtn = document.getElementById('search-btn');
-
+const creds = document.querySelector(".creds");
 searchBtn.addEventListener('click', () => {
     let location = document.getElementById('city-input').value;
     getWeather(location);
   });
-async function getWeather(location)
-{
-    try{
-
-        let call = await fetch(`https://api.weatherapi.com/v1/current.json?key=fe5bf2fd65fc420ea3c175649230906&q=${location}`);
-        let data = await call.json();
-        console.log(data);
-        const [localDate, localTime] = data.location.localtime.split(' ');
-        console.log(data.location.localtime);
-        console.log(data.location.name);
-        // console.log(data.current.feelslike_c);
-        // console.log(data.current.feelslike_f);
-        // console.log(data.current.humidity);
-        // console.log(data.current.temp_c);
-        // console.log(data.current.temp_f);
-        // console.log(data.current.wind_kph);
-        // console.log(data.current.condition.text);
-        // console.log(data.current.condition.icon);
-        document.querySelector('.location').textContent = data.location.name;
-        document.querySelector('.state').textContent = data.current.condition.text;
-        document.querySelector('.date').textContent = localDate ;
-        document.querySelector('.time').textContent = localTime
-        document.querySelector('.temperature .value').textContent = data.current.temp_c;
-        document.querySelector('.feels-like').textContent = `Feels Like: ${data.current.feelslike_c}°C`;
-        document.querySelector('.humidity').textContent = `Humidity: ${data.current.humidity}%`;
-        document.querySelector('.wind-speed').textContent = `Wind Speed: ${data.current.wind_kph} km/h`;
-
-
-
+  
+  async function getWeather(location) {
+    try {
+      const weatherApiKey = 'fe5bf2fd65fc420ea3c175649230906';
+      const pixabayApiKey = '33206537-0633c0c38beb775155d06f2a1';
+      const weatherUrl = `https://api.weatherapi.com/v1/current.json?key=${weatherApiKey}&q=${encodeURIComponent(location)}`;
+      const response = await fetch(weatherUrl);
+      const data = await response.json();
+      console.log(data);
+  
+      // Extract the necessary data from the API response
+      const weatherCondition = data.current.condition.text;
+      const localDate = data.location.localtime.split(' ')[0];
+      const localTime = data.location.localtime.split(' ')[1];
+      const temperature = data.current.temp_c;
+      const feelsLike = data.current.feelslike_c;
+      const humidity = data.current.humidity;
+      const windSpeed = data.current.wind_kph;
+  
+      // Update the DOM with the retrieved weather data
+      document.querySelector('.location').textContent = data.location.name;
+      document.querySelector('.state').textContent = weatherCondition;
+      document.querySelector('.date').textContent = localDate;
+      document.querySelector('.time').textContent = localTime;
+      document.querySelector('.temperature .value').textContent = temperature;
+      document.querySelector('.feels-like').textContent = `Feels Like: ${feelsLike}°C`;
+      document.querySelector('.humidity').textContent = `Humidity: ${humidity}%`;
+      document.querySelector('.wind-speed').textContent = `Wind Speed: ${windSpeed} km/h`;
+  
+      // Get the background image based on weather condition from Pixabay API
+      const pixabayUrl = `https://pixabay.com/api/?key=${pixabayApiKey}&q=${weatherCondition} weather&image_type=photo&category=nature`;
+      const pixabayResponse = await fetch(pixabayUrl);
+      const pixabayData = await pixabayResponse.json();
+      if (pixabayData.hits.length > 0) {
+        const imageUrl = pixabayData.hits[0].largeImageURL;
+        document.body.style.backgroundImage = `url(${imageUrl})`;
+      }
+    } catch (error) {
+      console.log('Error:', error);
     }
-    
-    catch{
-        console.log("ERROR");
-    }
-}
+  }
+  
+  // Default call
+  getWeather('London');
+  
+
+
